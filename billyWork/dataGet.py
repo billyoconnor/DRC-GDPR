d935e0cb3933f64601e92822c09aa39d34b4d6fd
billyoconnor <williamjpoconnor@gmail.com>
2020-08-25 23:06:07 +0100

    Semi-Final Updates and Improvements

--- billyWork/dataGet.py
+++ billyWork/dataGet.py
@@ -8,7 +8,7 @@ from pandas import read_excel
 from datetime import datetime
 
 Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
-tkinter.messagebox.showinfo(title="Upload", message="Please choose the workbook you wish to upload")# Popup window explaining about to ask for source workbook
+tkinter.messagebox.showinfo(title="Upload", message="Please choose the Assesment Export from OneTrust")# Popup window explaining about to ask for source workbook
 path1 = askopenfilename() # show an "Open" dialog box and return the path to the selected file - Store path as variable 'path1'
 
 
@@ -18,7 +18,7 @@ wb1.loc[wb1['Template'].str.contains("Security"),'Type'] = 'Security'
 wb1 = wb1.dropna(subset=['Type']) #remove the NaN values
 wb1.reset_index(drop=True) # reset the index count which would have been mucked up
 
-tkinter.messagebox.showinfo(title="Upload", message="Please Select the Risk Export Workbook")# Popup window explaining about to ask for source workbook
+tkinter.messagebox.showinfo(title="Upload", message="Please Select the Risk Export Workbook from OneTrust")# Popup window explaining about to ask for source workbook
 path3 = askopenfilename() # show an "Open" dialog box and return the path to the selected file - Store path as variable 'path2'
 wb4 = pd.read_excel(path3)
 
